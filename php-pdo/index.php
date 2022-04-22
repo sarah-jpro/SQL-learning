4eea6197b2ec00fb3e85d54fa6c643e46c0d4359
Sarah <sarahjonet.pro@gmail.com>
2022-04-22 10:53:34 +0200

    Ajout weatherapp

--- php-pdo/index.php
+++ php-pdo/index.php
@@ -6,57 +6,87 @@ try
 }
 catch(Exception $e)
 {
-	// En cas d'erreur, on affiche un message et on arrête tout
+	// En cas d'erreur, on affiche un message et stop
         die('Erreur : '.$e->getMessage());
 }
-$resultat = $bdd->query('SELECT * FROM météo');
-
-
-while ($donnees = $resultat->fetch())
-{
-//echo "<pre>";
-//print_r ($donnees);
-//echo "</pre>";
-}
-$resultat->closeCursor();
-
 ?>
-<form action="" method="get">
-        <label for="ville">Entrez une ville</label>
-        <input type="" name="ville"> <br>
-        <label for="haut">Entrez une température max</label>
-        <input type="" name="haut"> <br>
-        <label for="bas">Entrez une température min</label>
-        <input type="" name="bas"> <br>
-        <input type="checkbox" id="zone" name="ville" value="Bruxelles">
-        <label for="ville">Bruxelles</label><br>
-        <input type="checkbox" id="zone" name="ville" value="Liège">
-        <label for="ville">Liège</label><br>
-        <input type="checkbox" id="zone" name="ville" value="Namur">
-        <label for="ville">Namur</label><br>
-        <input type="checkbox" id="zone" name="ville" value="Charleroi">
-        <label for="ville">Charleroi</label><br>
-        <input type="checkbox" id="zone" name="ville" value="Bruges">
-        <label for="ville">Bruges</label><br>
-        <input type="submit">
-    </form>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>weatherapp</title>
+</head>
+<body>
+    <h1>weatherapp</h1>
+    <h2>Tableau HTML</h2>
 
     <?php
+    $resultat = $bdd->query('SELECT * FROM météo');
+    ?>
+
+<!--Création du tableau-->
+<form action="" method="post">
+        <table class="table">
+        <thead>
+            <tr>
+                <th></th>
+                <th>Ville</th>
+                <th>Température maximale</th>
+                <th>Température minimale</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php while($row = $resultat->fetch()) : ?>
+            <tr>
+                <td><input type="checkbox" name="del[]" value=<?php echo $row['ville'];?> </td>
+                <td><?php echo ($row['ville']); ?></td>
+                <td><?php echo ($row['haut']); ?></td>
+                <td><?php echo ($row['bas']); ?></td>
+            </tr>
+
+            <?php endwhile; 
+            $resultat->closeCursor();
+            ?>
+        </tbody>
+        </table>
+
+        <?php
+        if(isset($_POST['del'])) {
+            foreach($_POST['del'] as $value) {
+                $deleteRes = $bdd -> prepare("DELETE FROM météo WHERE ville = :ville");
+                $deleteRes -> execute([
+                    'ville' => $value,
+                ]);
+            }
+        }
+        ?>
+
+        <input type="submit" value="Supprimer une ville?" class="btn1">
+    </form>
+
+    <form action="" method="post">
+        <label for="ville">Ville</label>
+        <input type="" name="ville" pl> <br>
+        <label for="haut">Température max</label>
+        <input type="" name="haut"> <br>
+        <label for="bas">Température min</label>
+        <input type="" name="bas"> <br>
+        <input type="submit" value="Ajout ville" class="btn2">
+    </form>
+
+
+ 
+
+<?php
     //recup les infos du form 
-    $ville = isset($_GET['ville']) ? $_GET['ville'] : NULL;
-    $haut = isset($_GET['haut']) ? $_GET['haut'] : NULL;
-    $bas = isset($_GET['bas']) ? $_GET['bas'] : NULL;
-    echo $ville;
-try
-{
-	// On se connecte à MySQL
-	$bdd = new PDO('mysql:host=localhost;dbname=weatherapp;charset=utf8;port=3307', 'root', 'root');
-}
-catch(Exception $e)
-{
-	// En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
-}
+    $ville = isset($_POST['ville']) ? $_POST['ville'] : NULL;
+    $haut = isset($_POST['haut']) ? $_POST['haut'] : NULL;
+    $bas = isset($_POST['bas']) ? $_POST['bas'] : NULL;
+    //echo $ville;
+
 
 
 // Ecriture de la requête
@@ -69,9 +99,7 @@ $insertmétéo->execute([
     'haut' => $haut,
     'bas' => $bas,
     
-]);
-
+]) or die();
 
 $resultat -> closeCursor();
-
 ?>
